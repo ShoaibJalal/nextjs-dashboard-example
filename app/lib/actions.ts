b309d1099785f68fb5179e00332ba05deda82176
Shoaib Jalal <syedshoaibjalal@outlook.com>
2024-06-22 15:28:05 +0300

    Inserting data into database and redirecting a user

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,9 @@
 'use server';
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -7,11 +11,22 @@ const FormSchema = z.object({
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
